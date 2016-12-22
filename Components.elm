572a23b78ad7a19618418ac141090c222b31e54a
Ulric Wilfred <shaman.sir@gmail.com>
2016-12-22 23:31:45 +0100

    add first components

--- Components.elm
+++ Components.elm
@@ -1,1 +1,30 @@
 module Components exposing (..)
+
+{-| This module provides the visual representation
+for different types of values. It could be used in the Playground-like
+Editors.
+
+# List of the available components
+@docs stringComponent, numComponent
+
+-}
+
+import Html exposing (..)
+
+{-| Component for a `String`
+
+Usage:
+    stringComponent "foobar"
+-}
+stringComponent : String -> Html a
+stringComponent str =
+    span [] [ text str ]
+
+{-| Component for a `String`
+
+Usage:
+    numComponent 42
+-}
+numComponent : Int -> Html a
+numComponent num =
+    span [] [ text (toString num) ]
--- ComponentsDemo.elm
+++ ComponentsDemo.elm
@@ -1,7 +1,41 @@
-module ComponentsDemo exposing (..)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
-import Html exposing (Html, div, text)
+import Components
 
-main : Html a
 main =
-    div [] [ text "Hello World" ]
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+
+-- MODEL
+
+type alias Model = Int
+
+model : Model
+model =
+  0
+
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
