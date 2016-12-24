e4e673cbc877b16b10a148b9b9b6c1b8f3b728d7
Ulric Wilfred <shaman.sir@gmail.com>
2016-12-24 21:13:26 +0100

    add model for the list components

--- Components.elm
+++ Components.elm
@@ -23,73 +23,71 @@ Editors.
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-type alias StringComponentModel = String
-
 {-| Component for a `String`
 
 Usage:
     stringComponent "foobar"
 -}
-stringComponent : StringComponentModel -> Html a
+stringComponent : String -> Html a
 stringComponent str =
     span [ class "string" ] [ text str ]
-
-type alias IntComponentModel = Int
 
 {-| Component for an `Int`
 
 Usage:
     intComponent 42
 -}
-intComponent : IntComponentModel -> Html a
+intComponent : Int -> Html a
 intComponent num =
     span [ class "number int" ] [ text (toString num) ]
-
-type alias FloatComponentModel = Float
 
 {-| Component for a `Float`
 
 Usage:
     floatComponent 42.2
 -}
-floatComponent : FloatComponentModel -> Html a
+floatComponent : Float -> Html a
 floatComponent num =
     span [ class "number float" ] [ text (toString num) ]
 
-type alias StringListComponentModel = List String
+type alias ListModel x = (List x, Bool)
+
+type ListMsg = Expand | Collapse
+
+listUpdate : ListMsg -> ListModel x -> ListModel x
+listUpdate msg (list, expanded) =
+    case msg of
+        Expand -> (list, True)
+        Collapse -> (list, False)
 
 {-| Component for a `List String`
 
 Usage:
     stringListComponent [ "foo", "bar" ]
 -}
-stringListComponent : StringListComponentModel -> Html a
-stringListComponent stringList =
+stringListComponent : ListModel String -> Html ListMsg
+stringListComponent (stringList, expanded) =
     listOf stringComponent stringList
-
-type alias IntListComponentModel = List Int
 
 {-| Component for a `List Int`
 
 Usage:
     intListComponent [ 10, 12, 15, 55, 60 ]
 -}
-intListComponent : IntListComponentModel -> Html a
-intListComponent intList =
+intListComponent : ListModel Int -> Html ListMsg
+intListComponent (intList, expaned) =
     listOf intComponent intList
-
-type alias FloatListComponentModel = List Float
 
 {-| Component for a `List Float`
 
 Usage:
     floatListComponent [ 10.1, 12.2, 15.3, 55.4, 60.5, 111 ]
 -}
-floatListComponent : FloatListComponentModel -> Html a
-floatListComponent floatList =
+floatListComponent : ListModel Float -> Html ListMsg
+floatListComponent (floatList, expanded) =
     listOf floatComponent floatList
 
-listOf : (x -> Html a) -> List x -> Html a
+listOf : (x -> Html ListMsg) -> List x -> Html ListMsg
 listOf itemProducer items =
     ul [ class "list-of" ]
         (List.map (\item -> li [] [ (itemProducer item) ]) items)
